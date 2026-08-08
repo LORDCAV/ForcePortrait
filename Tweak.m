ff801248ea77280f581efe1677322eeb7fcccf31
LORDCAV <bs856b2q92@privaterelay.appleid.com>
2026-08-08 14:41:32 +0800

    Update Tweak.m

--- Tweak.m
+++ Tweak.m
@@ -1,6 +1,8 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
+#import <dlfcn.h>
 
+// Override UIKit responses
 UIInterfaceOrientationMask custom_supportedInterfaceOrientations(id self, SEL _cmd) {
     return UIInterfaceOrientationMaskPortrait;
 }
@@ -13,9 +15,15 @@ BOOL custom_shouldAutorotate(id self, SEL _cmd) {
     return YES;
 }
 
+// Force engine configuration requests to yield portrait values (Value 1 = Portrait)
+int custom_UnityGetTargetDesiredOrientation() {
+    return 1; 
+}
+
 __attribute__((constructor))
 static void init(void) {
     @autoreleasepool {
+        // 1. Hook the standard iOS base views
         Class class = [UIViewController class];
         
         Method method1 = class_getInstanceMethod(class, @selector(supportedInterfaceOrientations));
@@ -26,5 +34,12 @@ static void init(void) {
         
         Method method3 = class_getInstanceMethod(class, @selector(shouldAutorotate));
         class_replaceMethod(class, @selector(shouldAutorotate), (IMP)custom_shouldAutorotate, method_getTypeEncoding(method3));
+        
+        // 2. Intercept Unity's specific internal C-function mapping 
+        void *unity_func = dlsym(RTLD_DEFAULT, "UnityGetTargetDesiredOrientation");
+        if (unity_func != NULL) {
+            // Rebind the function pointer to force return values internally
+            class_replaceMethod(class, NSSelectorFromString(@"updateOrientation:"), (IMP)custom_UnityGetTargetDesiredOrientation, "v@:i");
+        }
     }
 }
